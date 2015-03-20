1b19a6e612b59dd44449aa6b6babb1367787493a
Steven Yue <jincheker@gmail.com>
2015-03-20 10:31:34 -0400

    Fixed the ugly indentation.

--- lib/base62-rb.rb
+++ lib/base62-rb.rb
@@ -1,35 +1,35 @@
 require "base62/version"
 
 module Base62
-	KEYS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	KEYS_HASH = KEYS.each_char.with_index.inject({}){ |h,(k,v)| h[k]=v; h }
-	BASE = KEYS.length
+  KEYS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  KEYS_HASH = KEYS.each_char.with_index.inject({}){ |h,(k,v)| h[k]=v; h }
+  BASE = KEYS.length
 
-	# Encodes base10 (decimal) number to base62 string.
-	def self.encode(num)
-		return "0" if num == 0
-		return nil if num < 0
+  # Encodes base10 (decimal) number to base62 string.
+  def self.encode(num)
+    return "0" if num == 0
+    return nil if num < 0
 
-		str = ""
-		while num > 0
-			# prepend base62 charaters
-			str = KEYS[num % BASE] + str
-			num = num / BASE
-		end
-		return str
-	end
+    str = ""
+    while num > 0
+      # prepend base62 charaters
+      str = KEYS[num % BASE] + str
+      num = num / BASE
+    end
+    return str
+  end
 
-	# Decodes base62 string to a base10 (decimal) number.
-	def self.decode(str)
-		num = 0
-		i = 0
-		len = str.length - 1
-		# while loop is faster than each_char or other 'idiomatic' way
-		while i < str.length
-			pow = BASE ** (len - i)
-			num += KEYS_HASH[str[i]] * pow
-			i += 1
-		end
-		return num
-	end
+  # Decodes base62 string to a base10 (decimal) number.
+  def self.decode(str)
+    num = 0
+    i = 0
+    len = str.length - 1
+    # while loop is faster than each_char or other 'idiomatic' way
+    while i < str.length
+      pow = BASE ** (len - i)
+      num += KEYS_HASH[str[i]] * pow
+      i += 1
+    end
+    return num
+  end
 end
